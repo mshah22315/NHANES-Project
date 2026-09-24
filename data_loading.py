704100f29f86aacf4fbcc6156b72eacc74f13900
Manthan Shah <manthanshah22@outlook.com>
2026-09-24 15:22:17 -0500

    fixed up data loading script

--- data_loading.py
+++ data_loading.py
@@ -1,16 +1,16 @@
 import pandas as pd
 
-years = [2015, 2017]
-files = ["DEMO_I", "DEMO_J", "IMQ_I", "IMQ_J", "HEQ_I", "HEQ_J", "MCQ_I", "MCQ_J", "KIQ_I", "KIQ_J", "HEPBD_I", "HEPBD_J", "HEPB_S_I", "HEPB_S_J"]
+files_2015 = ["DEMO_I", "IMQ_I", "HEQ_I", "MCQ_I", "KIQ_U_I", "HEPBD_I", "HEPB_S_I"]
+files_2017 = ["DEMO_J", "IMQ_J", "HEQ_J", "MCQ_J", "KIQ_U_J", "HEPBD_J", "HEPB_S_J"]
 
-def load_data(years, files):
-    for y in years:
-        for f in files:
-            url = f"https://wwwn.cdc.gov/Nchs/Data/Nhanes/Public/{y}/DataFiles/{f}.xpt"
-            print(f"Loading {f} from {y}")
-            df = pd.read_sas(url, format='xport', encoding='utf-8')
-            print(f"Saving {f} from {y} to CSV")
-            df.to_csv(f"data/{f}_{y}.csv", index=False)
+def load_data(year, files):
+    for f in files:
+        url = f"https://wwwn.cdc.gov/Nchs/Data/Nhanes/Public/{year}/DataFiles/{f}.xpt"
+        print(f"Loading {f} from {year}")
+        df = pd.read_sas(url, format='xport', encoding='utf-8')
+        print(f"Saving {f} from {year} to CSV")
+        df.to_csv(f"data/{f}_{year}.csv", index=False)
 
 if __name__ == "__main__":
-    load_data(years, files)
+    load_data(2015, files_2015)
+    load_data(2017, files_2017)
