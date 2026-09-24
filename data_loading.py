1b013f6a50d3f92c2a1d260b146794323b1bd26f
Manthan Shah <manthanshah22@outlook.com>
2026-09-24 15:10:13 -0500

    adjusted code

--- data_loading.py
+++ data_loading.py
@@ -2,9 +2,15 @@ import pandas as pd
 
 years = [2015, 2017]
 files = ["DEMO_I", "DEMO_J", "IMQ_I", "IMQ_J", "HEQ_I", "HEQ_J", "MCQ_I", "MCQ_J", "KIQ_I", "KIQ_J", "HEPBD_I", "HEPBD_J", "HEPB_S_I", "HEPB_S_J"]
-demo_url = "https://wwwn.cdc.gov/Nchs/Data/Nhanes/Public/2017/DataFiles/DEMO_J.xpt"
-for year in years:
-    for file in files:
-        url = f"https://wwwn.cdc.gov/Nchs/Data/Nhanes/Public/{year}/DataFiles/{file}.xpt"
-        df = pd.read_sas(url, format='xport', encoding='utf-8')
-        df.to_csv(f"data/{file}_{year}.csv", index=False)
+
+def load_data(years, files):
+    for y in years:
+        for f in files:
+            url = f"https://wwwn.cdc.gov/Nchs/Data/Nhanes/Public/{y}/DataFiles/{f}.xpt"
+            print(f"Loading {f} from {y}")
+            df = pd.read_sas(url, format='xport', encoding='utf-8')
+            print(f"Saving {f} from {y} to CSV")
+            df.to_csv(f"data/{f}_{y}.csv", index=False)
+
+if __name__ == "__main__":
+    load_data(years, files)
