d10532af6160ffba828a843a30dda50eb11b77af
Manthan Shah <manthanshah22@outlook.com>
2026-09-24 16:23:38 -0500

    added more files, adjusted directory save path

--- data_loading.py
+++ data_loading.py
@@ -1,7 +1,8 @@
 import pandas as pd
+import os
 
-files_2015 = ["DEMO_I", "IMQ_I", "HEQ_I", "MCQ_I", "KIQ_U_I", "HEPBD_I", "HEPB_S_I"]
-files_2017 = ["DEMO_J", "IMQ_J", "HEQ_J", "MCQ_J", "KIQ_U_J", "HEPBD_J", "HEPB_S_J"]
+files_2015 = ["DEMO_I", "IMQ_I", "DUQ_I", "HEQ_I", "HIQ_I", "MCQ_I", "KIQ_U_I", "HEPBD_I", "HEPB_S_I"]
+files_2017 = ["DEMO_J", "IMQ_J", "DUQ_J", "HEQ_J", "HIQ_J", "MCQ_J", "KIQ_U_J", "HEPBD_J", "HEPB_S_J"]
 
 def load_data(year, files):
     for f in files:
@@ -9,7 +10,8 @@ def load_data(year, files):
         print(f"Loading {f} from {year}")
         df = pd.read_sas(url, format='xport', encoding='utf-8')
         print(f"Saving {f} from {year} to CSV")
-        df.to_csv(f"data/{f}_{year}.csv", index=False)
+        os.makedirs(f"data/{year}", exist_ok=True)
+        df.to_csv(f"data/{year}/{f}.csv", index=False)
 
 if __name__ == "__main__":
     load_data(2015, files_2015)
